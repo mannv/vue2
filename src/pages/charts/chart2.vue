ebb7b0772c325a6faad062de93be91fa1374cee4
mannv <mannv@vnext.com.vn>
2021-04-08 11:19:41 +0700

    cm

--- src/pages/charts/chart2.vue
+++ src/pages/charts/chart2.vue
@@ -1,11 +1,103 @@
 <template>
   <div>
     <h1>Demo 3djs Chart 2</h1>
+    <svg width="960" height="600"></svg>
   </div>
 </template>
 
 <script>
-export default {}
+/* eslint-disable */
+import * as d3 from 'd3'
+export default {
+  mounted() {
+    let marge = { top: 60, bottom: 60, left: 60, right: 60 }
+    let svg = d3.select('svg')
+    let width = svg.attr('width')
+    let height = svg.attr('height')
+    let g = svg
+      .append('g')
+      .attr('transform', 'translate(' + marge.top + ',' + marge.left + ')')
+
+    let dataset = [10, 20, 30, 23, 13, 40, 27, 35, 20]
+
+    // const xScaleValue = [
+    //   'thang 01',
+    //   'thang 02',
+    //   'thang 03',
+    //   'thang 04',
+    //   'thang 05',
+    //   'thang 06',
+    //   'thang 06',
+    //   'thang 07',
+    //   'thang 08',
+    //   'thang 09',
+    //   'thang 10',
+    //   'thang 11',
+    //   'thang 12',
+    //   'thang 13',
+    //   'thang 14',
+    //   'thang 15',
+    //   'thang 16',
+    //   'thang 17',
+    //   'thang 18',
+    //   'thang 19',
+    //   'thang 20',
+    //   'thang 20',
+    //   'thang 22',
+    //   'thang 23',
+    //   'thang 24',
+    //   'thang 25',
+    // ]
+
+    // x , y 方向绘制坐标轴
+    let xScale = d3
+      .scaleBand()
+      // .domain(xScaleValue)
+      .domain(d3.range(dataset.length))
+      .rangeRound([0, width - marge.left - marge.right])
+    let xAxis = d3.axisBottom(xScale)
+
+    let yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataset)])
+      .range([height - marge.top - marge.bottom, 0])
+    let yAxis = d3.axisLeft(yScale)
+
+    g.append('g')
+      .attr(
+        'transform',
+        'translate(' + 0 + ',' + (height - marge.top - marge.bottom) + ')'
+      )
+      .call(xAxis)
+
+    // g.append('g').attr('transform', 'translate(0, 0)').call(yAxis)
+
+    let gs = g.selectAll('.rect').data(dataset).enter().append('g')
+
+    // 绘制矩形
+    let rectPadding = 20
+    gs.append('rect')
+      .attr('x', function (d, i) {
+        return xScale(i) + rectPadding / 2
+      })
+      .attr('y', function (d) {
+        return yScale(d)
+      })
+      .attr('width', function () {
+        return xScale.step() - rectPadding
+      })
+      .attr('height', function (d) {
+        return height - marge.top - marge.bottom - yScale(d)
+      })
+      .attr('fill', 'lightblue')
+      .on('mouseover', function () {
+        d3.select(this).transition().duration(100).attr('fill', 'yellow')
+      })
+      .on('mouseout', function () {
+        d3.select(this).transition().duration(100).attr('fill', 'lightblue')
+      })
+  },
+}
 </script>
 
 <style scoped></style>
