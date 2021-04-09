f32c7cd8c536266e9d70a391d0cf9668fab4868e
mannv <mannv@vnext.com.vn>
2021-04-09 17:55:36 +0700

    add

--- src/pages/charts/chart1.vue
+++ src/pages/charts/chart1.vue
@@ -2,13 +2,23 @@
   <div>
     <h1>Demo 3djs Chart 1 22</h1>
     <canvas></canvas>
+    <button class="btn btn-default" @click="demoTransition">Transition</button>
+    <button class="btn btn-primary" @click="enterData">enterData</button>
+    <button class="btn btn-danger" @click="addBlock">add Block</button>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 import * as d3 from 'd3'
 // import { axisLeft } from 'd3-axis'
 export default {
+  data() {
+    return {
+      chart: null,
+      bar1: null,
+    }
+  },
   mounted() {
     // const svg = d3
     //   .create('svg')
@@ -29,35 +39,96 @@ export default {
     //   })
 
     // const axis = d3.axisLeft(1)
-    // d3.select('canvas')
+    // this.chart = d3.select('canvas')
     //   .append('svg')
     //   .attr('width', 1440)
     //   .attr('height', 30)
     //   .append('g')
     //   .attr('transform', 'translate(0,30)')
-    //   .call(axis)
 
-    let canvas = d3
+    this.chart = d3
       .select('body')
       .append('svg')
       .attr('width', 500)
       .attr('height', 500)
       .style('background', 'gray')
 
-    canvas
+    this.chart
       .append('circle')
       .attr('cx', 50)
       .attr('cy', 50)
       .attr('r', 50)
       .style('fill', 'green')
+    //
 
-    canvas
+    this.chart
       .append('ellipse')
       .attr('cx', 400)
       .attr('cy', 50)
       .attr('rx', 100)
       .attr('ry', 50)
       .style('fill', '#000')
+      .on('mouseover', function () {
+        d3.select(this).transition().duration(500).style('fill', 'orange')
+      })
+      .on('mouseout', function () {
+        d3.select(this).transition().duration(500).style('fill', '#000')
+      })
+
+    this.bar1 = this.chart
+      .append('rect')
+      .attr('fill', 'orange')
+      .attr('x', 100)
+      .attr('y', 20)
+      .attr('height', 20)
+      .attr('width', 10)
+
+    // let data = [100, 200, 400]
+    // let canvas = d3
+    //   .selectAll('canvas')
+    //   .data(data)
+    //   .text((value, index) => {
+    //     console.log('value: %s, index: %s', value, index)
+    //     return value
+    //   })
+  },
+  methods: {
+    demoTransition() {
+      console.log('d3.easeLinear', d3.easeLinear)
+      this.bar1
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(500)
+        .delay(2000)
+        .attr('height', 400)
+    },
+    enterData() {
+      const data = ['text1', 'text2', 'thang 4']
+      console.log('data', data)
+      d3.select('h1')
+        .selectAll('span')
+        .data(data)
+        .enter()
+        .append('span')
+        .text(function (d) {
+          return `text ${d}`
+        })
+        .style('color', function (d, i) {
+          if (i % 2 === 0) {
+            return 'red'
+          }
+          return 'blue'
+        })
+    },
+    addBlock() {
+      this.chart
+        .append('rect')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', 200)
+        .attr('height', 200)
+        .attr('fill', 'blue')
+    },
   },
 }
 </script>
--- src/pages/charts/chart2.vue
+++ src/pages/charts/chart2.vue
@@ -8,9 +8,18 @@
 <script>
 /* eslint-disable */
 import * as d3 from 'd3'
+import { scaleBand, scaleLinear, scaleOrdinal } from 'd3-scale'
 export default {
+  data() {
+    return {
+      yScale: 1,
+    }
+  },
   mounted() {
-    let marge = { top: 60, bottom: 60, left: 60, right: 60 }
+    this.yScale = d3.scaleLinear()
+    console.log('this.yScale', this.yScale)
+
+    let marge = { top: 60, bottom: 0, left: 60, right: 60 }
     let svg = d3.select('svg')
     let width = svg.attr('width')
     let height = svg.attr('height')
@@ -18,42 +27,52 @@ export default {
       .append('g')
       .attr('transform', 'translate(' + marge.top + ',' + marge.left + ')')
 
-    let dataset = [10, 20, 30, 23, 13, 40, 27, 35, 20]
+    const yGridFunction = d3.axisBottom(this.yScale).tickSize(height)
+    // .ticks(this.cfg.axis.yTicks, this.cfg.axis.yFormat)
 
-    // const xScaleValue = [
-    //   'thang 01',
-    //   'thang 02',
-    //   'thang 03',
-    //   'thang 04',
-    //   'thang 05',
-    //   'thang 06',
-    //   'thang 06',
-    //   'thang 07',
-    //   'thang 08',
-    //   'thang 09',
-    //   'thang 10',
-    //   'thang 11',
-    //   'thang 12',
-    //   'thang 13',
-    //   'thang 14',
-    //   'thang 15',
-    //   'thang 16',
-    //   'thang 17',
-    //   'thang 18',
-    //   'thang 19',
-    //   'thang 20',
-    //   'thang 20',
-    //   'thang 22',
-    //   'thang 23',
-    //   'thang 24',
-    //   'thang 25',
-    // ]
+    g.attr(
+      'transform',
+      'horizontal' ? 'translate(0,0)' : `translate(0,${height})`
+    )
+      .transition(this.transition)
+      .call(yGridFunction)
+
+    let dataset = [10, 20, 30, 23, 13, 40, 27, 35, 20, 22, 22, 222, 222]
+
+    const xScaleValue = [
+      'thang 01',
+      'thang 02',
+      'thang 03',
+      'thang 04',
+      'thang 05',
+      'thang 06',
+      'thang 06',
+      'thang 07',
+      'thang 08',
+      'thang 09',
+      'thang 10',
+      'thang 11',
+      'thang 12',
+      'thang 13',
+      'thang 14',
+      'thang 15',
+      'thang 16',
+      'thang 17',
+      'thang 18',
+      'thang 19',
+      'thang 20',
+      'thang 20',
+      'thang 22',
+      'thang 23',
+      'thang 24',
+      'thang 25',
+    ]
 
     // x , y 方向绘制坐标轴
     let xScale = d3
       .scaleBand()
-      // .domain(xScaleValue)
-      .domain(d3.range(dataset.length))
+      .domain(xScaleValue)
+      // .domain(d3.range(dataset.length))
       .rangeRound([0, width - marge.left - marge.right])
     let xAxis = d3.axisBottom(xScale)
 
@@ -75,7 +94,7 @@ export default {
     let gs = g.selectAll('.rect').data(dataset).enter().append('g')
 
     // 绘制矩形
-    let rectPadding = 20
+    let rectPadding = 50
     gs.append('rect')
       .attr('x', function (d, i) {
         return xScale(i) + rectPadding / 2
@@ -89,12 +108,12 @@ export default {
       .attr('height', function (d) {
         return height - marge.top - marge.bottom - yScale(d)
       })
-      .attr('fill', 'lightblue')
+      .attr('fill', '#1976d2')
       .on('mouseover', function () {
         d3.select(this).transition().duration(100).attr('fill', 'yellow')
       })
       .on('mouseout', function () {
-        d3.select(this).transition().duration(100).attr('fill', 'lightblue')
+        d3.select(this).transition().duration(100).attr('fill', '#1976d2')
       })
   },
 }
