9d39ff2686f539bab5e2e0512a449c4477316cc8
mannv <mannv@vnext.com.vn>
2021-04-26 17:13:05 +0700

    cm

--- src/components/CKEditor.vue
+++ src/components/CKEditor.vue
@@ -4,6 +4,7 @@
       v-model="editorData"
       :config="editorConfig"
       :disabled="disabled"
+      editor-url="https://cdn.ckeditor.com/4.14.0/full-all/ckeditor.js"
     ></ckeditor>
     <pre>{{ editorData }}</pre>
   </div>
@@ -16,11 +17,11 @@ export default {
       disabled: false,
       editorData: `
       <h1 style="color: red; font-size: 20px; background: green;a:1" class="demo1">Demo H1</h1>
-    <h2 style="color: red; background: green;a:1" class="demo2">Demo H2</h2>
-    <h3 style="color: red; background: green;a:1" class="demo3"><strong>Demo</strong> H3</h3>
-    <h4 style="color: red; background: green;a:1" class="demo4">Demo H4</h4>
-    <h5 style="color: red; background: green;a:1" class="demo5">Demo H5</h5>
-    <h6 style="color: red; background: green;a:1" class="demo6">Demo H6</h6>
+    <h2 style="color: red;" class="demo2">Demo H2</h2>
+    <h3 style="color: red; background: green;" class="demo3"><strong>Demo</strong> H3</h3>
+    <h4 style="color: red;" class="demo4">Demo H4</h4>
+    <h5 style="color: red;" class="demo5">Demo H5</h5>
+    <h6 style="color: lightseagreen;" class="demo6">Demo H6</h6>
 
     <p style="background: blue; padding-top: 10px;">Demo P with style</p>
 
@@ -32,15 +33,18 @@ export default {
       </tr>
     </table>`,
       editorConfig: {
-        // toolbar: ['bold', 'italic', '|', 'link'],
+        format_tags: 'p;h1;h2;h3;h4;h5;h6',
         toolbar: [
-          {
-            name: 'document',
-            items: ['Source'],
-          },
+          { name: 'document', items: ['Source'] },
+          { name: 'styles', items: ['Format', 'Font', 'FontSize'] },
+          { name: 'colors', items: ['TextColor', 'BGColor'] },
         ],
-        allowedContent: 'h1{*}; h2{*}; h3{*}; h4{*}; h5{*}; h6{*}; p{*}; br',
-        // disallowedContent: 'a',
+        // language: 'ja',
+        height: 400,
+        // allowedContent: 'h1{*}; h2{*}; h3{*}; h4{*}; h5{*}; h6{*}; span{*}',
+        allowedContent:
+          'h1{*}; h2{*}; h3{*}; h4{*}; h5{*}; h6{*}; p{*}; span{*}; br;',
+        // disallowedContent: 'span',
       },
     }
   },
